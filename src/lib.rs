a9683c8ed1bb257af8849f992b1674d102c4de5d
Kyle Partridge <kyle.partridge@sendgrid.com>
2018-12-03 11:49:05 -0800

    convert to using i32::from_str_radix

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 pub mod lib {
     pub fn handle_frequency_line(line: Result<String, std::io::Error>) -> Result<i32, String> {
         let line = line.map_err(|e| e.to_string())?;
-        let num = &line[1..].parse::<i32>().map_err(|e| e.to_string())?;
-        match &line[0..1] {
-            "+" => Ok(num.clone()),
-            "-" => Ok(-num.clone()),
-            _ => Err(format!("invalid frequency: {}", &line[0..1]))
-        }
+        i32::from_str_radix(&line[..], 10).map_err(|e| e.to_string())
     }
 }
